692a860fe2ce118c50263154d6bfb91042017903
u-kosmonaft-u <59250755+u-kosmonaft-u@users.noreply.github.com>
2020-12-05 09:36:32 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test', bootstrap_servers='192.168.10.50:9092')
-
+consumer = KafkaConsumer(group_id='pythonGroup',
+                         bootstrap_servers=['192.168.10.60:9092', '192.168.10.61:9092', '192.168.10.62:9092'])
+consumer.subscribe(['pythonTopic'])
 for msg in consumer:
     print(msg)
